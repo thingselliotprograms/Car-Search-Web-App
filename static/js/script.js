9a100963f45ff751400e4bfe203c21fbe97fe568
Elliot Watson <nostawtoill.e@gmail.com>
2017-04-03 03:09:22 -0500

    Script so far

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,24 @@
-
+$(document).ready(function () {
+    $('.select-maker').hide(),
+    $('#select-by-maker').click(function () {
+        $('.select-maker').show();
+    }),
+    $('#sort-by-quality').click(function () {
+        $('.select-maker').hide();
+    }),
+    $(function () {
+        $('.carmakers').bind('click', function () {
+            $('#maseratimodels').text()
+            $.getJSON('/modelssamepage', {
+                maker: $(this).text()
+            }, function (data) {
+                $('#maseratimodels').empty()
+                console.log(data);
+                $.each(data, function (key, value) {
+                    $('#maseratimodels').append("<li class='list-group-item'><h3>" + value + "</h3></li>")
+                });
+            })
+            return false;
+        });
+    })
+    });
